784951546e6d95ccc9e0d33ab23d66b176eb43ca
solojungle <aliawari.email@gmail.com>
2021-05-14 14:21:43 -0400

    Starting apps service, working on redis

--- redis/redis.go
+++ redis/redis.go
@@ -1,1 +1,31 @@
 package redis
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis"
+
+	"main.go/config"
+)
+
+type RedisClient struct {
+	client *redis.Client
+}
+
+func NewRedisClient(cfg config.Config) *RedisClient {
+	// Get address from config
+	redisAddress := fmt.Sprintf("%v:%v", cfg.Redis.Address, cfg.Redis.Port)
+
+	// Connection to redis
+	client := redis.NewClient(&redis.Options{
+		Addr:     redisAddress,
+		Password: "",
+		DB:       0,
+	})
+
+	return &RedisClient{client}
+}
+
+func (r *RedisClient) Ping() (string, error) {
+	return r.client.Ping().Result()
+}
